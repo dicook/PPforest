5ff8e98eef990873c3399f208dbec2e606f7a6ce
natydasilva <ndasilva@iastate.edu>
2014-09-22 23:54:30 -0500

    functions name changes

--- R/boots.samp.R
+++ R/boots.samp.R
@@ -1,11 +1,11 @@
-sample.tr<-function(data,ntree){
+sample.train<-function(data,ntree){
   n<-length(data[, 1])
   sam.id<-rdply(ntree,sample(1:n, replace = TRUE) )[,-1]
   cbind(tr=rep(1:ntree,each=n),data[as.numeric(data.matrix(sam.id)),] )  
 }
 
 
-boots.samp<-function(i.class, i.data,m.index,PPmethod='LDA', weight = TRUE, r = NULL,  
+pps.index<-function(i.class, i.data,m.index,PPmethod='LDA', weight = TRUE, r = NULL,  
                      lambda, cooling = 0.999, temp = 1, energy = 0.01,  ...)
 {
   n<-length(i.data[, 1])
@@ -28,8 +28,8 @@ boots.samp<-function(i.class, i.data,m.index,PPmethod='LDA', weight = TRUE, r = 
 bagg.pp<-function(data,ntree,m.index,PPmethod='LDA', weight = TRUE, r = NULL,  
                   lambda, cooling = 0.999, temp = 1, energy = 0.01,  ...){
   
-  aux<-sample.tr(data,ntree)
-  dlply(aux,.(tr),function(x) boots.samp(i.class=x[,2],i.data=x[,-c(1,2)],m.index))
+  aux<-sample.train(data,ntree)
+  dlply(aux,.(tr),function(x) pps.index(i.class=x[,2],i.data=x[,-c(1,2)],m.index))
 }
 
 
