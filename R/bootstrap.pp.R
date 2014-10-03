14ce94783ab4696024a1791b01e04200dcddf349
Dianne Cook <visnut@gmail.com>
2014-10-03 13:45:09 -0500

    formatting and handling of training indices

--- R/bootstrap.pp.R
+++ R/bootstrap.pp.R
@@ -1,6 +1,7 @@
 #' List of PP.Tree objects for bootstrap samples.
 #'
 #' @param data is a data frame with the complete data set. Class factor in the first column
+#' @param aux indices of the training data set
 #' @param ntree number of bootstrap samples or individual classifiers (trees).
 #' @param size.p proportion of sample in each class group.
 #' @param index PP index to run the PP.Tree, options LDA, Lp or PDA.
@@ -10,11 +11,10 @@
 #' @examples
 #' data<-iris[,5:1]
 #' output<-bootstrap_pp(data,ntree=100,size.p=.9,index="LDA")  
-bootstrap_pp <- function(data,ntree,size.p,index='LDA', ...){
-  aux <- train_fn(data[,1],size.p)
+bootstrap_pp <- function(data, aux, ntree, size.p, index='LDA', ...){
   out <- mlply(data.frame(tr=1:ntree), function(tr) {
     boot <- sort(sample(aux, replace = TRUE))
-    pp.tree<-PP.Tree(PPmethod=index,i.data=data[boot,-1],i.class=data[boot,1],...) 
+    pp.tree <- PP.Tree(PPmethod=index,i.data=data[boot,-1],i.class=data[boot,1],...) 
     list(boot,pp.tree)
   })
   return(list(out,aux))        
--- R/train_fn.R
+++ R/train_fn.R
@@ -6,10 +6,11 @@
 #' @export
 #' @examples
 #' train_fn(iris[,5],.9)
-train_fn <- function(class,size.p){
+train_fn <- function(class, size.p) {
   n <- length(class)
   class.id <- data.frame(id=1:n,class=class)
-  index  <- unlist(dlply(class.id,.(class),function(x) sample(x$id,round(length(x$class)*size.p,1))))
+  index  <- unlist(dlply(class.id, .(class), function(x) 
+    sample(x$id, round(length(x$class)*size.p, 1))))
   names(index) <- NULL
   return(sort(index))
 }
