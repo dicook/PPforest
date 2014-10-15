68a1b83d641fbcf3524d67fd143583d1d4ad6916
natydasilva <ndasilva@iastate.edu>
2014-10-15 15:54:49 -0500

    example

--- Example/examples.R
+++ Example/examples.R
@@ -1,4 +1,5 @@
 
+#example using Iris data
 
 data.iris<-iris[,5:1]
 training<-train_fn(iris[,5],.9)
@@ -8,10 +9,15 @@ result.boot<-mlply(data.frame(ntree=c(1,10,50,100,500)), function(ntree) {
 } )  
 
 
-
-
-result.bagg<-llply(result.boot[1:5],bagging_pp,data=data,result=result.boot)
+result.bagg<-llply(result.boot[1:5],bagging_pp,data=data.iris,result=result.boot)
 
 error<-sapply(result.bagg,function(x) x[[1]][1] )
 aux<-c(1,10,50,100,500)
-xtable(cbind(aux,error),digits=5)
+(cbind(aux,error)
+
+ 
+ #Example olive data
+ setwd("./Data")
+ load("olive.rda")
+head(d.olive)
+
