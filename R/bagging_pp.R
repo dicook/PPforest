fd8e2f5a266b63eac1c117d6b6f9b8311d078cb4
natydasilva <ndasilva@iastate.edu>
2014-10-16 22:43:21 -0500

    oob error

--- R/bagging_pp.R
+++ R/bagging_pp.R
@@ -7,21 +7,39 @@
 #' @examples
 #' data1<-iris[,5:1]
 #' output<-bootstrap_pp(data1,training,ntree=50,index="LDA")     
-#' bagging_pp(data1, output)
-bagging_pp <- function(data, boot, ...){
-  votes.tr <- ldply(boot[[1]], function(x) PP.classify(test.data=data[,-1],
-                                true.class=data[,1], x[[2]], Rule=1)$predict.class)
+#' bagging_pp(data1, output,training)
+bagging_pp <- function(data, boot,training, ...){
+  votes <- ldply(boot[[1]], function(x) PP.classify(test.data=data[,-1],
+                                true.class=data[,1], x[[2]], Rule=1)$predict.class)[,-1]
   error.tr <- ldply(boot[[1]], function(x) PP.classify(test.data=data[,-1],
                                 true.class=data[,1], x[[2]], Rule=1)$predict.error)
-  max.vote <- apply(votes.tr[,-1], 2, function(x) {
+  max.vote <- apply(votes, 2, function(x) {
         t1 <- table(x)
     names(t1)[which.max(t1)]
     }
   )
  
+  v.train <- votes[,training]
+  l.train <- 1:length(training)
+  oob.var <- ldply(boot[[1]],function(x) data.frame(t(!l.train%in%x[[1]])))[,-1]
+  
+  mv.train <- melt(v.train)
+  moob.var <- melt(oob.var)
+ oob.votes <-mv.train[moob.var$value,]
+   max.oob <- ddply(oob.votes,.(variable),function(x){
+        t1 <- table(x$value)
+    names(t1)[which.max(t1)]
+    }
+  )
+
+ tr.class <- data.frame(paste(variable="V",training,sep=""),V1=as.numeric(data[training,1]))
+ cond <- tr.class[max.oob[,1],]
+   oob.error <- (dim(cond)[1]-sum(cond[,2]==as.numeric(max.oob[,2])))/dim(cond)[1]
+  
             tab.t <- table(Observed=data[,1],Predicted=max.vote)
   colnames(tab.t) <- rownames(tab.t)
-            tab.p <- round(prop.table(tab.t,1),7)
+ class.error <- 1-diag(tab.t)/((addmargins(tab.t,2))[,"Sum"])
+            tab.p <- cbind(round(prop.table(tab.t,1),7),class.error)
             error <- round((dim(data)[1]-sum(diag(tab.t)))/dim(data)[1],5)
- return(list(error, as.numeric(max.vote),tab.p))
+ return(list(oob.error,error, as.numeric(max.vote),tab.p))
 } 
