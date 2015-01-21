94a3e88d4d9d5bf10accd93e9cd1e64f9d88ebd8
natydasilva <ndasilva@iastate.edu>
2015-01-20 22:35:01 -0600

    proximity, first and second column of the object proximity indicates the position (row and column) in the lower tirngular proximity matrix

--- R/bagging_pp.R
+++ R/bagging_pp.R
@@ -5,14 +5,15 @@
 #' @param strata is to stratify the bootstrap samples, by default is TRUE.
 #' @param boot object from bootstrap_pp (list of PP.Tree objets for bootstrap samples)
 #' @param training indices of the training data set.
+#' @param ntree number of bootstrap samples or individual classifiers (trees).
 #' @return list with the error, predicted classes and confusion matrix.
 #' @export
 #' @examples
 #' data1<-iris[,5:1]
 #' training<-train_fn(iris[,5],.9)
 #' output<-bootstrap_pp(data1,scale=TRUE,size.p=.9,training,strata=TRUE,ntree=50,index="LDA")      
-#' b.pp <- bagging_pp(data1,scale=TRUE, strata=TRUE,output,training)
-bagging_pp <- function(data, scale=TRUE,strata=TRUE,boot,training, ...){
+#' b.pp <- bagging_pp(data1,scale=TRUE, strata=TRUE,output,training,ntree=50)
+bagging_pp <- function(data, scale=TRUE,strata=TRUE,boot,training,ntree, ...){
   if(strata==TRUE) data[,-1] <- scale(data[,-1])
   votes <- plyr::ldply(boot[[1]], function(x) PPtree::PP.classify(test.data=data[,-1],
                                 true.class=data[,1], x[[2]], Rule=1)$predict.class)[,-1]
@@ -23,6 +24,13 @@ bagging_pp <- function(data, scale=TRUE,strata=TRUE,boot,training, ...){
     }
   )
 
+  
+  pos <- expand.grid(a=1:dim(data)[1], b=1:dim(data)[1])
+  cond <- pos[,1]>= pos[,2]
+  tri.low <- pos[cond,]
+  same.node <- data.frame(tri.low,dif=apply(t(votes),2,function(x) x[ tri.low[,1]]-x[ tri.low[,2]]))
+  proximity <- data.frame(same.node[,c(1:2)],proxi=apply(same.node[,-c(1:2)],1,function(x) sum(x==0))/ntree)
+  
   
  votes.con <- apply(votes,2,table)
  votes.prop <- ldply(votes.con,function(x) as.data.frame(x/dim(votes)[1]))
@@ -66,7 +74,7 @@ oob.error <- 1-sum(diag(tab.oob))/length(cond[,1])
  class.error <- 1-diag(tab.t)/((addmargins(tab.t,2))[,"Sum"])
             tab.p <- cbind(round(prop.table(tab.t,1),7),class.error)
             error <- round((dim(data[training,])[1]-sum(diag(tab.t)))/dim(data)[1],5)
-out <- list(oob.error,error,as.numeric(max.vote),tab.p,oob.error.tree$V1,votes,votes.prop)
-names(out) <-c("OOB estimate or error rate","Training error","Predicted","Confusion matrix","OOB error Tree","Votes","Votes proportion")
+out <- list(oob.error,error,as.numeric(max.vote),tab.p,oob.error.tree$V1,votes,votes.prop,proximity)
+names(out) <-c("OOB estimate or error rate","Training error","Predicted","Confusion matrix","OOB error Tree","Votes","Votes proportion","Proximity")
 return(out)
 } 
