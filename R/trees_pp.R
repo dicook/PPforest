082105b8311fe11a403b659d15c816e132c0297d
natydasilva <ndasilva@iastate.edu>
2015-04-09 00:36:52 -0500

    inlcude lambda for PDA

--- R/trees_pp.R
+++ R/trees_pp.R
@@ -13,11 +13,13 @@
 #' output1 <- trees_pp(iris.b,size.p=.9,index="LDA") 
 #' output2 <- trees_pp(iris.b,size.p=.9,index="PDA",lambda=.14) 
 #' 
-trees_pp <- function(data.b,size.p=.9,index='LDA', ...){
+trees_pp <- function(data.b,size.p=.9,index='LDA',lambda,...){
   names(data.b)[1] <-"class"
   if(index=="LDA") {
   trees <- data.b %>%
                   dplyr::do(tr = LDAtree_split(as.formula('class~.'), data = . ,size.p=size.p,...))
+               #dplyr::do(tr = PPtree_split2(index, fr=as.formula('class~.'), data = . ,size.p=size.p,...))
+  
   }else
   {trees <- data.b %>%
      dplyr::do(tr = PDAtree_split(as.formula('class~.'), data = . ,size.p=size.p,lambda,...))
