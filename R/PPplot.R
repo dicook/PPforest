1bf4df6448b0b55556de22ec5b9670caa24a2b81
natydasilva <ndasilva@iastate.edu>
2015-05-12 10:54:24 -0500

    check

---
+++ R/PPplot.R
@@ -0,0 +1,40 @@
+#' MDS
+#'
+#' @param ppfo  a PPforest object
+#' @param train is a data frame with the training data with class in the first column
+#' @param k number of MDS dimensions to plot
+#' @return proximity matrix plot and MDS plot  
+#' @export
+#' @importFrom magrittr %>%
+#' @examples
+#' tr.index <- train_fn(iris[, 5], 2/3)
+#' te.index <- as.vector(1:length(iris[, 5]))[!(1:length(iris[, 5]) %in% (sort(tr.index$id)))]
+#' train <- iris[sort(tr.index$id), 5:1 ]
+#' test <- iris[-tr.index$id, 5:1 ]
+#' ppfr.iris <- PPforest( train = train, testap = TRUE, test = test, m = 500, size.p = .9, PPmethod = 'LDA', strata = TRUE)
+#' PPplot(ppfr.iris, train, k = 2)
+#' PPplot(ppfr.iris, train, k = 3)
+
+
+PPplot <- function(ppfo, train, k) {
+  id <- diag(dim(train)[1])
+  id[lower.tri(id, diag=TRUE)] <- ppfo[[8]]$proxi
+  id[upper.tri(id)] <-  t(id)[upper.tri(id)]
+  ggplot2::ggplot(reshape2::melt(id), ggplot2::aes(Var1,Var2, fill=value)) + ggplot2::geom_raster()
+  
+  rf.mds <- stats::cmdscale(1 - id, eig = TRUE, k = k)
+  colnames(rf.mds$points) <- paste("Dim", 1:k, sep='')
+  nlevs <- nlevels(train[,1])
+  if (k <= 2) {
+    df <- data.frame(fac=train[,1], rf.mds$points)
+    ggplot2::ggplot(data= df, ggplot2::aes(x=Dim1, y =Dim2,color= train[,1])) +ggplot2::scale_colour_discrete(name="Class")  + ggplot2::geom_jitter()
+  }
+  else {
+    df <- data.frame(fac=train[,1], rf.mds$points)
+    GGally::ggpairs(data=df[,-1], colours=as.factor(train[,1]))
+  }
+  
+}
+
+
+
---
+++ R/train_fn.R
@@ -0,0 +1,17 @@
+#' Index id for training set, sample in each class with the same proportion.
+#' 
+#' @param class input character vector with class of the data
+#' @param size.p proportion of sample in each class group
+#' @return numeric vector  with the sample indexes for training data
+#' @export
+#' @importFrom magrittr %>%
+#' @examples
+#' data(iris)
+#' training <- train_fn(class=iris[,5], size.p=0.9)
+#' training
+train_fn <- function(class = class, size.p = 0.9) {
+    n <- length(class)
+    class.id <- data.frame(id = 1:n, class = class)
+    class.id %>% dplyr::group_by(class) %>% dplyr::sample_frac(size.p) %>% dplyr::arrange(id) %>% dplyr::ungroup() %>% 
+        dplyr::select(id)
+} 
