85cb08051487bf2fa2ff3d6079c3123aa6c2e67e
natydasilva <ndasilva@iastate.edu>
2015-05-30 20:43:26 -0500

    importance variable plot for each node

--- R/importance.R
+++ R/importance.R
@@ -14,24 +14,36 @@
 importance <- function(data, ppforest) {
   value <- NULL
   variable <- NULL
-  mat.vi <- abs(plyr::ldply(ppforest[[8]][[2]], function(x) data.frame(dim =  1:dim(x$projbest.node)[1],x$projbest.node)))
+  mat.vi <- abs(plyr::ldply(ppforest[[8]][[2]], function(x) data.frame(node =  1:dim(x$projbest.node)[1],x$projbest.node)))
   colnames(mat.vi)[-1] <- colnames(data[, -1])
 
-  oob.error.tree <- rep(ppforest[[6]], each = length(unique(mat.vi$dim)))
+  oob.error.tree <- rep(ppforest[[6]], each = length(unique(mat.vi$node)))
   imp.weight <- mat.vi[,-1]*(1 - oob.error.tree)
-  mmat.vi <- reshape2::melt(mat.vi, id.vars = "dim")
-  mat.vi.w <- data.frame(dim=mat.vi$dim, imp.weight)
+  mmat.vi <- reshape2::melt(mat.vi, id.vars = "node")
+  mat.vi.w <- data.frame(node=mat.vi$node, imp.weight)
   colnames(mat.vi.w)[-1] <- colnames(data[, -1])
-  mmat.vi.w <- reshape2::melt(mat.vi.w, id.vars = "dim")
+  mmat.vi.w <- reshape2::melt(mat.vi.w, id.vars = "node")
   
   mmat.vi.0 <- subset(mmat.vi, value != 0)
   mmat.vi.w.0 <- subset(mmat.vi.w, value != 0)
   
-  print(ggplot2::qplot(data=mmat.vi.0,x=variable,y=value,geom="boxplot",facets = ~dim) + 
-          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)))
+  import.vi.0 <- mmat.vi.0 %>%
+  group_by(node,variable) %>%
+  summarise(mean = mean(value)) %>%
+    arrange(desc(mean)) 
+ 
+  import.vi.w.0 <- mmat.vi.w.0 %>%
+    group_by(node,variable) %>%
+    summarise(mean = mean(value)) %>%
+    arrange(desc(mean)) 
+   
+  ggplot( import.vi.w.0,aes(x=mean,y=variable))+geom_point()+facet_wrap(~node)
   
-  print(ggplot2::qplot(data = mmat.vi.w.0, x = variable,y = value, geom = "boxplot", facets = ~dim) + 
-          ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)))
+  #print(ggplot2::qplot(data=mmat.vi.0,x=variable,y=value,geom="boxplot",facets = ~ node) + 
+   #       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)))
+  
+  #print(ggplot2::qplot(data = mmat.vi.w.0, x = variable,y = value, geom = "boxplot", facets = ~ node) + 
+   #       ggplot2::theme(axis.text.x = ggplot2::element_text(angle = 90, hjust = 1)))
   return(list(mat.vi, mat.vi.w))
 }
 
