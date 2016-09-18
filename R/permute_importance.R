0525d189449fedee0debdeeecf556666a1694126
natydasilva <ndasilva@iastate.edu>
2016-09-17 20:38:01 -0500

    Importance variable computed by permuted variables like RF

---
+++ R/permute_importance.R
@@ -0,0 +1,73 @@
+#' Obtain the importance variable measure based in permutation
+#' 
+#' @param ppf is a PPforest object
+#' @param cl.tree when it is TRUE compute the importance variable base on permuted oob observations for each variable when it is FALSE compute an importance for all the forest with permuted values for each variable instead
+#' @return importance variable plot  
+#' @export
+#' @importFrom magrittr %>%
+permute_importance <- function(ppf, cl.tree = TRUE){
+  imp <-NULL
+  nm <- NULL
+  oob.id <- apply(ppf$oob.obs,1, which)
+  permute <- plyr:: llply(oob.id , function(x) sample(x,length(x)))
+if(cl.tree){
+  pred.oob.per <- list()
+  corr.oob.per <- matrix(0,ncol=ncol(ppf$train)-1,nrow=length(permute)) 
+for(i in 1:ncol(ppf$train)-1) {
+
+  for (j in 1:length(permute)) {
+    d <- ppf$train[ oob.id[[j]], ]
+    d[, i+1] <- ppf$train[ permute[[j]], i+1]
+    
+    
+    pred.oob.per[[j]] <- PPtreeViz::PP.classify(test.data = d[,-1], Tree.result =  ppf[[8]][[2]][[j]], Rule = 1)[[2]]
+    corr.oob.per[j,i] <-  sum(diag(table(pred.oob.per[[j]], d[, class])))
+  }
+}
+  corr.oob <- (1-ppf$oob.error.tree) * unlist(lapply(oob.id, length))
+  
+ imp.pl<- data.frame( nm = colnames(ppf$train)[colnames(ppf$train)!=ppf$class.var], imp=apply(corr.oob.per, 2, function(x) mean((corr.oob-x)))) %>% dplyr::arrange(imp)
+ imp.pl$nm <- factor(imp.pl$nm, levels= imp.pl[order( imp.pl$imp), "nm"])
+ 
+ p <-  ggplot2::ggplot(data = imp.pl, ggplot2::aes(imp,nm) ) + ggplot2::geom_point() + ggplot2::xlab("Importance") +
+   ggplot2::ylab("")
+  plotly::ggplotly(p)
+}else{
+  
+  
+  #For each variable a permutation is done without consider oob or not, the new data are predicted with the forest
+  #and the training error is computed.
+  
+  
+  corr.per <- numeric()
+  for (j in 1:(ncol(ppf$train)-1)) {
+   
+    d <- ppf$train[,colnames(ppf$train)[colnames(ppf$train)!=ppf$class.var]]
+    rnd <- sample(1:nrow(ppf$train), nrow(ppf$train) )
+    d[, j] <- d[rnd, j]
+    corr.per[j] <- sum(PPforest::predict( object = ppf, class= "Type", test.data = d)[[2]] == ppf$train[,ppf$class.var])
+  }
+  
+  
+ 
+  
+  corr.oob <- (1-ppf$oob.error.forest) * nrow(ppf$train)
+  
+  imp.pl<- data.frame( nm=colnames(ppf$train)[colnames(ppf$train)!=ppf$class.var], imp=corr.oob-corr.per) %>% dplyr::arrange(imp)
+  imp.pl$nm <- factor(imp.pl$nm, levels= imp.pl[order( imp.pl$imp), "nm"])
+  
+  p <-  ggplot2::ggplot(data = imp.pl, ggplot2::aes(imp,nm) ) +ggplot2::geom_point() + ggplot2::xlab("Importance") +
+    ggplot2::ylab("")
+  plotly::ggplotly(p)
+  
+}
+  
+}
+  
+  
+ 
+  
+  
+
+ 
+
